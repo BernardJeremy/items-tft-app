7629ed19280302214fa5425a3b2fc4c4b58752ad
Jeremy Bernard <jeremy@bernard.sh>
2025-04-12 15:39:59 +0200

    add items data

---
+++ src/app/itemsData.ts
@@ -0,0 +1,183 @@
+export const itemsData = {
+    components: [
+        { id: "bf_sword", label: "B.F. Sword", src: "https://rerollcdn.com/items/BFSword.png" },
+        { id: "recurve_bow", label: "Recurve Bow", src: "https://rerollcdn.com/items/RecurveBow.png" },
+        { id: "needlessly_large_rod", label: "Needlessly Large Rod", src: "https://rerollcdn.com/items/NeedlesslyLargeRod.png" },
+        { id: "tear_of_the_goddess", label: "Tear of the Goddess", src: "https://rerollcdn.com/items/TearoftheGoddess.png" },
+        { id: "chain_vest", label: "Chain Vest", src: "https://rerollcdn.com/items/ChainVest.png" },
+        { id: "negatron_cloak", label: "Negatron Cloak", src: "https://rerollcdn.com/items/NegatronCloak.png" },
+        { id: "giants_belt", label: "Giant's Belt", src: "https://rerollcdn.com/items/GiantsBelt.png" },
+        { id: "sparring_gloves", label: "Sparring Gloves", src: "https://rerollcdn.com/items/SparringGloves.png" },
+        { id: "spatula", label: "Spatula", src: "https://rerollcdn.com/items/Spatula.png" },
+        { id: "frying_pan", label: "Frying Pan", src: "https://rerollcdn.com/items/FryingPan.png" },
+    ],
+    complete: [
+        { id: "deathblade", label: "Deathblade", src: "https://rerollcdn.com/items/Deathblade.png" },
+        { id: "giant_slayer", label: "Giant Slayer", src: "https://rerollcdn.com/items/GiantSlayer.png" },
+        { id: "hextech_gunblade", label: "Hextech Gunblade", src: "https://rerollcdn.com/items/HextechGunblade.png" },
+        { id: "spear_of_shojin", label: "Spear of Shojin", src: "https://rerollcdn.com/items/SpearofShojin.png" },
+        { id: "edge_of_night", label: "Edge of Night", src: "https://rerollcdn.com/items/EdgeofNight.png" },
+        { id: "bloodthirster", label: "Bloodthirster", src: "https://rerollcdn.com/items/Bloodthirster.png" },
+        { id: "steadfast", label: "Steadfast Heart", src: "https://rerollcdn.com/items/SteadfastHeart.png" },
+        { id: "infinity_edge", label: "Infinity Edge", src: "https://rerollcdn.com/items/InfinityEdge.png" },
+        { id: "rageblade", label: "Guinsoo's Rageblade", src: "https://rerollcdn.com/items/GuinsoosRageblade.png" },
+        { id: "statikk_shiv", label: "Statikk Shiv", src: "https://rerollcdn.com/items/StatikkShiv.png" },
+        { id: "jeweled_gauntlet", label: "Jeweled Gauntlet", src: "https://rerollcdn.com/items/JeweledGauntlet.png" },
+        { id: "blue_buff", label: "Blue Buff", src: "https://rerollcdn.com/items/BlueBuff.png" },
+        { id: "bramble_vest", label: "Bramble Vest", src: "https://rerollcdn.com/items/BrambleVest.png" },
+        { id: "dragons_claw", label: "Dragon's Claw", src: "https://rerollcdn.com/items/DragonsClaw.png" },
+        { id: "warmogs", label: "Warmog's Armor", src: "https://rerollcdn.com/items/WarmogsArmor.png" },
+        { id: "titans_resolve", label: "Titan's Resolve", src: "https://rerollcdn.com/items/TitansResolve.png" },
+        { id: "tacticians_shield", label: "Tacticians Shield", src: "https://rerollcdn.com/items/TacticiansShield.png" },
+        { id: "tacticians_cape", label: "Tacticians Cape", src: "https://rerollcdn.com/items/TacticiansCape.png" },
+        { id: "tacticians_crown", label: "Tacticians Crown", src: "https://rerollcdn.com/items/TacticiansCrown.png" },
+    ],
+    emblems: [
+        {
+            label: "Bastion Emblem",
+            id: "bastion_emblem",
+            image: "https://rerollcdn.com/items/BastionEmblem.png"
+        },
+        {
+            label: "Golden Ox Emblem",
+            id: "golden_ox_emblem",
+            image: "https://rerollcdn.com/items/GoldenOxEmblem.png"
+        },
+        {
+            label: "Anima Squad Emblem",
+            id: "anima_squad_emblem",
+            image: "https://rerollcdn.com/items/AnimaSquadEmblem.png"
+        },
+        {
+            label: "BoomBots Emblem",
+            id: "boombots_emblem",
+            image: "https://rerollcdn.com/items/BoomBotsEmblem.png"
+        },
+        {
+            label: "Exotech Emblem",
+            id: "exotech_emblem",
+            image: "https://rerollcdn.com/items/ExotechEmblem.png"
+        },
+        {
+            label: "Marksman Emblem",
+            id: "marksman_emblem",
+            image: "https://rerollcdn.com/items/MarksmanEmblem.png"
+        },
+        {
+            label: "Techie Emblem",
+            id: "techie_emblem",
+            image: "https://rerollcdn.com/items/TechieEmblem.png"
+        },
+        {
+            label: "Rapidfire Emblem",
+            id: "rapidfire_emblem",
+            image: "https://rerollcdn.com/items/RapidfireEmblem.png"
+        },
+        {
+            label: "Street Demon Emblem",
+            id: "street_demon_emblem",
+            image: "https://rerollcdn.com/items/StreetDemonEmblem.png"
+        },
+        {
+            label: "Slayer Emblem",
+            id: "slayer_emblem",
+            image: "https://rerollcdn.com/items/SlayerEmblem.png"
+        },
+        {
+            label: "Divinicorp Emblem",
+            id: "divinicorp_emblem",
+            image: "https://rerollcdn.com/items/DivinicorpEmblem.png"
+        },
+        {
+            label: "Dynamo Emblem",
+            id: "dynamo_emblem",
+            image: "https://rerollcdn.com/items/DynamoEmblem.png"
+        },
+        {
+            label: "Bruiser Emblem",
+            id: "bruiser_emblem",
+            image: "https://rerollcdn.com/items/BruiserEmblem.png"
+        },
+        {
+            label: "Strategist Emblem",
+            id: "strategist_emblem",
+            image: "https://rerollcdn.com/items/StrategistEmblem.png"
+        },
+        {
+            label: "Executioner Emblem",
+            id: "executioner_emblem",
+            image: "https://rerollcdn.com/items/ExecutionerEmblem.png"
+        },
+        {
+            label: "Syndicate Emblem",
+            id: "syndicate_emblem",
+            image: "https://rerollcdn.com/items/SyndicateEmblem.png"
+        }
+    ],
+    combos: {
+        "bf_sword+bf_sword": "deathblade",
+        "bf_sword+recurve_bow": "giant_slayer",
+        "bf_sword+needlessly_large_rod": "hextech_gunblade",
+        "bf_sword+tear_of_the_goddess": "spear_of_shojin",
+        "bf_sword+chain_vest": "edge_of_night",
+        "bf_sword+negatron_cloak": "bloodthirster",
+        "bf_sword+giants_belt": "steadfast",
+        "bf_sword+sparring_gloves": "infinity_edge",
+        "bf_sword+spatula": "slayer_emblem",
+        "bf_sword+frying_pan": "executioner_emblem",
+        
+        "recurve_bow+recurve_bow": "rageblade",
+        "recurve_bow+needlessly_large_rod": "statikk_shiv",
+        "recurve_bow+tear_of_the_goddess": "titans_resolve",
+        "recurve_bow+chain_vest": "titans_resolve",
+        "recurve_bow+negatron_cloak": "runaans_hurricane",
+        "recurve_bow+giants_belt": "titans_resolve",
+        "recurve_bow+sparring_gloves": "last_whisper",
+        "recurve_bow+spatula": "marksman_emblem",
+        "recurve_bow+frying_pan": "rapidfire_emblem",
+        
+        "needlessly_large_rod+needlessly_large_rod": "jeweled_gauntlet",
+        "needlessly_large_rod+tear_of_the_goddess": "blue_buff",
+        "needlessly_large_rod+chain_vest": "locket_of_iron_solari",
+        "needlessly_large_rod+negatron_cloak": "ionic_spark",
+        "needlessly_large_rod+giants_belt": "morellonomicon",
+        "needlessly_large_rod+sparring_gloves": "jeweled_gauntlet",
+        "needlessly_large_rod+spatula": "exotech_emblem",
+        "needlessly_large_rod+frying_pan": "divinicorp_emblem",
+        
+        "tear_of_the_goddess+tear_of_the_goddess": "blue_buff",
+        "tear_of_the_goddess+chain_vest": "frozen_heart",
+        "tear_of_the_goddess+negatron_cloak": "chalice_of_power",
+        "tear_of_the_goddess+giants_belt": "redemption",
+        "tear_of_the_goddess+sparring_gloves": "hand_of_justice",
+        "tear_of_the_goddess+spatula": "strategist_emblem",
+        "tear_of_the_goddess+frying_pan": "dynamo_emblem",
+        
+        "chain_vest+chain_vest": "bramble_vest",
+        "chain_vest+negatron_cloak": "gargoyle_stoneplate",
+        "chain_vest+giants_belt": "sunfire_cape",
+        "chain_vest+sparring_gloves": "shroud_of_stillness",
+        "chain_vest+spatula": "bastion_emblem",
+        "chain_vest+frying_pan": "syndicate_emblem",
+        
+        "negatron_cloak+negatron_cloak": "dragons_claw",
+        "negatron_cloak+giants_belt": "zephyr",
+        "negatron_cloak+sparring_gloves": "quicksilver",
+        "negatron_cloak+spatula": "golden_ox_emblem",
+        "negatron_cloak+frying_pan": "street_demon_emblem",
+        
+        "giants_belt+giants_belt": "warmogs",
+        "giants_belt+sparring_gloves": "thiefs_gloves",
+        "giants_belt+spatula": "bruiser_emblem",
+        "giants_belt+frying_pan": "boombots_emblem",
+        
+        "sparring_gloves+sparring_gloves": "thiefs_gloves",
+        "sparring_gloves+spatula": "anima_squad_emblem",
+        "sparring_gloves+frying_pan": "techie_emblem",
+        
+        "spatula+spatula": "tacticians_crown",
+        "spatula+frying_pan": "tacticians_crown",
+        
+        "frying_pan+frying_pan": "tacticians_shield",
+    }
+};
